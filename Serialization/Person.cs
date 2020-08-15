83e6f0f2f927dac90b70ccb42d7433be750a459b
Stefan Gherman <scgheman@gmail.com>
2020-08-15 05:21:31 +0300

    Add sample Person class

--- Serialization/Person.cs
+++ Serialization/Person.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace Serialization
 {
-    class Person
+    [Serializable]
+    public class Person
     {
-        private string name;
-        private int age;
-        private Gender personGender;
-        private DateTime birthDate;
-
+       
+       
 
         public string Name { get; set; }
         public int Age { get; private set; }
         public Gender PersonGender { get; set; }
         public DateTime BirthDate { get; set; }
+
+        public Person(string name, Gender gender, DateTime birthDate)
+        {
+            Name = name;
+            PersonGender = gender;
+            BirthDate = birthDate;
+            CalculateAge();
+        }
+        private void CalculateAge()
+        {
+            Age = DateTime.Now.Year - BirthDate.Year;
+            if (Age < 0 )
+            {
+                Age = 0;
+            }
+           
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} is a {PersonGender} born on {BirthDate} ({Age} years old).";
+        }
     }
 }
