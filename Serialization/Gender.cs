1e5e4b4853c3f14f201e9424a6440f78661a1dd4
Stefan Gherman <scgheman@gmail.com>
2020-08-15 04:12:44 +0300

    Add project files.

---
+++ Serialization/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serialization
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ Serialization/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serialization
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private Gender personGender;
+        private DateTime birthDate;
+
+
+        public string Name { get; set; }
+        public int Age { get; private set; }
+        public Gender PersonGender { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
